2c27736d6bddce09506a613ffe47032e3931d7fe
tsctsc6 <tsctsc6@126.com>
2025-09-05 11:45:12 +0800

    生成迁移

---
+++ MusicManagementDemo.Infrastructure/Migrations/Music/20250905033935_2025090501_MusicInitial.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NpgsqlTypes;
+
+#nullable disable
+
+namespace MusicManagementDemo.Infrastructure.Migrations.Music
+{
+    /// <inheritdoc />
+    public partial class _2025090501_MusicInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "music");
+
+            migrationBuilder.CreateTable(
+                name: "MusicInfo",
+                schema: "music",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Artist = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Album = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    FilePath = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    TitleTSV = table.Column<NpgsqlTsVector>(type: "TSVECTOR", nullable: false, computedColumnSql: "to_tsvector('english', \"Title\")", stored: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MusicInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MusicInfo_TitleTSV",
+                schema: "music",
+                table: "MusicInfo",
+                column: "TitleTSV")
+                .Annotation("Npgsql:IndexMethod", "GIN");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MusicInfo",
+                schema: "music");
+        }
+    }
+}
