33c3c28cad3afc3c71e19dc966cf9a5071cfda92
tsctsc6 <tsctsc6@126.com>
2025-09-07 18:03:58 +0800

    添加实体

---
+++ MusicManagementDemo.Domain/Entity/Music/MusicInfoMusicListMap.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicManagementDemo.Domain.Entity.Music;
+
+public class MusicInfoMusicListMap : SharedKernel.Entity
+{
+    public Guid MusicInfoId { get; set; }
+    
+    public Guid MusicListId { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+}
---
+++ MusicManagementDemo.Domain/Entity/Music/MusicList.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicManagementDemo.Domain.Entity.Music;
+
+public class MusicList : SharedKernel.Entity
+{
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; } = string.Empty;
+    
+    public Guid UserId { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+}
