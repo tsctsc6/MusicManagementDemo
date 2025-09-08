2089a39fee410aa07e36611f22e3857e458ea84b
tsctsc6 <tsctsc6@126.com>
2025-09-08 13:04:43 +0800

    添加web api

---
+++ MusicManagementDemo.WebApi/Endpoints/Management/DeleteStorage.cs
@@ -0,0 +1,27 @@
+﻿
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using MusicManagementDemo.Application.UseCase.Management.DeleteStorage;
+
+namespace MusicManagementDemo.WebApi.Endpoints.Management;
+
+internal sealed class DeleteStorage : IEndpoint
+{
+    private sealed record Request(int Id);
+
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost(
+                "api/management/delete-storage",
+                async (Request request, IMediator mediator, CancellationToken cancellationToken) =>
+                {
+                    var result = await mediator.Send(
+                        new DeleteStorageCommand(Id: request.Id),
+                        cancellationToken
+                    );
+                    return Results.Ok(result);
+                }
+            )
+            .RequireAuthorization(new AuthorizeAttribute { Roles = "Admin" });
+    }
+}
