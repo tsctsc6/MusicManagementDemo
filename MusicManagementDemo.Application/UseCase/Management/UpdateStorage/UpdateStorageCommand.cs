0d3c7b796ef72a6ce23456ce18726197ade3db16
tsctsc6 <tsctsc6@126.com>
2025-09-08 13:25:31 +0800

    添加webapi

--- MusicManagementDemo.Application/UseCase/Management/UpdateStorage/UpdateStorageCommand.cs
+++ MusicManagementDemo.Application/UseCase/Management/UpdateStorage/UpdateStorageCommand.cs
@@ -3,5 +3,5 @@ using MusicManagementDemo.SharedKernel;
 
 namespace MusicManagementDemo.Application.UseCase.Management.UpdateStorage;
 
-internal sealed record UpdateStorageCommand(int Id, string Name, string Path)
+public sealed record UpdateStorageCommand(int Id, string Name, string Path)
     : IRequest<IServiceResult>;
---
+++ MusicManagementDemo.WebApi/Endpoints/Management/UpdateStorage.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using MusicManagementDemo.Application.UseCase.Management.UpdateStorage;
+
+namespace MusicManagementDemo.WebApi.Endpoints.Management;
+
+internal sealed class UpdateStorage : IEndpoint
+{
+    private sealed record Request(int Id, string Name, string Path);
+
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost(
+                "api/management/update-storage",
+                async (Request request, IMediator mediator, CancellationToken cancellationToken) =>
+                {
+                    var result = await mediator.Send(
+                        new UpdateStorageCommand(
+                            Id: request.Id,
+                            Name: request.Name,
+                            Path: request.Path
+                        ),
+                        cancellationToken
+                    );
+                    return Results.Ok(result);
+                }
+            )
+            .RequireAuthorization(new AuthorizeAttribute { Roles = "Admin" });
+    }
+}
