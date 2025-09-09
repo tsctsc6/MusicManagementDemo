11777edc47c814d6da4c6e9014cd347827a84cb5
tsctsc6 <tsctsc6@126.com>
2025-09-09 11:00:28 +0800

    给第一个角色赋予管理员角色

--- MusicManagementDemo.Application/UseCase/Identity/Register/RegisterCommandHandler.cs
+++ MusicManagementDemo.Application/UseCase/Identity/Register/RegisterCommandHandler.cs
@@ -1,12 +1,14 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using MusicManagementDemo.Application.Responses;
 using MusicManagementDemo.Domain.Entity.Identity;
+using MusicManagementDemo.Infrastructure.Database;
 using MusicManagementDemo.SharedKernel;
 
 namespace MusicManagementDemo.Application.UseCase.Identity.Register;
 
-internal sealed class RegisterCommandHandler(UserManager<ApplicationUser> userMgr)
+internal sealed class RegisterCommandHandler(UserManager<ApplicationUser> userMgr, IdentityAppDbContext dbContext)
     : IRequestHandler<RegisterCommand, IServiceResult>
 {
     public async Task<IServiceResult> Handle(
@@ -14,11 +16,22 @@ internal sealed class RegisterCommandHandler(UserManager<ApplicationUser> userMg
         CancellationToken cancellationToken
     )
     {
+        var isFirstUser = !await dbContext.Users.AnyAsync(cancellationToken: cancellationToken);
         var user = new ApplicationUser { UserName = request.UserName, Email = request.Email };
         var result = await userMgr.CreateAsync(user, request.Password);
         if (!result.Succeeded)
         {
             return ServiceResult.Err(503, [.. result.Errors.Select(e => e.Description)]);
+        }
+
+        if (isFirstUser)
+        {
+            var adminRole = await dbContext.Roles.SingleOrDefaultAsync(e => e.NormalizedName == "ADMIN");
+            if (adminRole is null)
+            {
+                return ServiceResult.Err(503, ["内部错误"]);
+            }
+            await userMgr.AddToRoleAsync(user, adminRole.NormalizedName!);
         }
         return ServiceResult<Guid>.Ok(Guid.Parse(user.Id));
     }
