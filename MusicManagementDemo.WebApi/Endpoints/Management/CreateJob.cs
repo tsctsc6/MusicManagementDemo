751351da6cb37353b08c02aadb0f0d7f64bb064c
tsctsc6 <tsctsc6@126.com>
2025-09-09 14:28:37 +0800

    添加webapi

---
+++ MusicManagementDemo.WebApi/Endpoints/Management/CreateJob.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using MusicManagementDemo.Application.UseCase.Management.CreateJob;
+using MusicManagementDemo.SharedKernel;
+
+namespace MusicManagementDemo.WebApi.Endpoints.Management;
+
+internal sealed class CreateJob : IEndpoint
+{
+    private sealed record Request(JobType Type, string Description);
+
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost(
+                "api/management/create-job",
+                async (Request request, IMediator mediator, CancellationToken cancellationToken) =>
+                {
+                    var result = await mediator.Send(
+                        new CreateJobCommand(request.Type, request.Description),
+                        cancellationToken
+                    );
+                    return Results.Ok(result);
+                }
+            )
+            .RequireAuthorization(new AuthorizeAttribute { Roles = "Admin" });
+    }
+}
