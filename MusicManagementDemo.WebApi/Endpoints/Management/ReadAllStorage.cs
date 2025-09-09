40da525ee591bd4d4846f8e0fadfb9651f77620a
tsctsc6 <tsctsc6@126.com>
2025-09-09 11:33:22 +0800

    添加webapi

---
+++ MusicManagementDemo.WebApi/Endpoints/Management/ReadAllStorage.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using MusicManagementDemo.Application.UseCase.Management.ReadAllStorage;
+
+namespace MusicManagementDemo.WebApi.Endpoints.Management;
+
+internal sealed class ReadAllStorage : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet(
+                "api/management/read-all-storage",
+                async (int page, int pageSize, IMediator mediator, CancellationToken cancellationToken) =>
+                {
+                    var result = await mediator.Send(
+                        new ReadAllStorageQuery(Page: page, PageSize: pageSize),
+                        cancellationToken
+                    );
+                    return Results.Ok(result);
+                }
+            )
+            .RequireAuthorization(new AuthorizeAttribute { Roles = "Admin" });
+    }
+}
