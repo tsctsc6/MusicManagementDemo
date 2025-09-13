2631bdbd7baed776f97ee4cd587769d916319662
tsctsc6 <tsctsc6@126.com>
2025-09-13 14:42:44 +0800

    添加认证权限

--- MusicManagementDemo.WebApi/Endpoints/Music/GetMusicStream.cs
+++ MusicManagementDemo.WebApi/Endpoints/Music/GetMusicStream.cs
@@ -10,24 +10,28 @@ internal sealed class GetMusicStream : IEndpoint
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
         app.MapGet(
-            "api/music/get-music-stream/{musicStreamId:guid}",
-            async (Guid musicStreamId, IMediator mediator, CancellationToken cancellationToken) =>
-            {
-                var optionalStream = await mediator.Send(
-                    new GetMusicStreamQuery(musicStreamId),
-                    cancellationToken
-                );
-                return optionalStream switch
+                "api/music/get-music-stream/{musicStreamId:guid}",
+                async (
+                    Guid musicStreamId,
+                    IMediator mediator,
+                    CancellationToken cancellationToken
+                ) =>
                 {
-                    SomeOption<Stream> stream => Results.File(
-                        fileStream: stream.Value,
-                        contentType: "video/x-flac",
-                        enableRangeProcessing: true
-                    ),
-                    _ => Results.NotFound(),
-                };
-            }
-        );
-        //.RequireAuthorization(new AuthorizeAttribute());
+                    var optionalStream = await mediator.Send(
+                        new GetMusicStreamQuery(musicStreamId),
+                        cancellationToken
+                    );
+                    return optionalStream switch
+                    {
+                        SomeOption<Stream> stream => Results.File(
+                            fileStream: stream.Value,
+                            contentType: "video/x-flac",
+                            enableRangeProcessing: true
+                        ),
+                        _ => Results.NotFound(),
+                    };
+                }
+            )
+            .RequireAuthorization(new AuthorizeAttribute());
     }
 }
