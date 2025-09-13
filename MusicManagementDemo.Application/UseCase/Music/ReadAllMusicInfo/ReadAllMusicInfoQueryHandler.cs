a8d43515a1a023306f48d1179cdc122614e36c30
tsctsc6 <tsctsc6@126.com>
2025-09-13 16:34:24 +0800

    搜索结果按照搜索相关性排序

--- MusicManagementDemo.Application/UseCase/Music/ReadAllMusicInfo/ReadAllMusicInfoQueryHandler.cs
+++ MusicManagementDemo.Application/UseCase/Music/ReadAllMusicInfo/ReadAllMusicInfoQueryHandler.cs
@@ -1,6 +1,5 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using MusicManagementDemo.Abstractions;
 using MusicManagementDemo.Abstractions.IDbContext;
 using MusicManagementDemo.Application.Responses;
@@ -18,29 +17,45 @@ internal sealed class ReadAllMusicInfoQueryHandler(IMusicAppDbContext dbContext)
         var musicInfosToReadQuery = dbContext.MusicInfo.AsQueryable();
         if (!string.IsNullOrWhiteSpace(request.SearchTerm))
         {
-            musicInfosToReadQuery = musicInfosToReadQuery.Where(m =>
-                m.TitleTSV.Matches(EF.Functions.PlainToTsQuery(dbContext.MusicInfoTitleTsConfig, request.SearchTerm))
-            );
-        }
-
-        if (request.Asc)
-        {
-            musicInfosToReadQuery = musicInfosToReadQuery.OrderBy(e => e.Id);
-            if (request.ReferenceId is not null)
-            {
-                musicInfosToReadQuery = musicInfosToReadQuery.Where(e =>
-                    e.Id > request.ReferenceId
+            musicInfosToReadQuery = musicInfosToReadQuery
+                .Where(m =>
+                    m.TitleTSV.Matches(
+                        EF.Functions.ToTsQuery(
+                            NpgsqlValues.TsConfigSimple,
+                            request.SearchTerm.Replace(" ", "|")
+                        )
+                    )
+                )
+                .OrderByDescending(m =>
+                    m.TitleTSV.Rank(
+                        EF.Functions.ToTsQuery(
+                            NpgsqlValues.TsConfigSimple,
+                            request.SearchTerm.Replace(" ", "|")
+                        )
+                    )
                 );
-            }
         }
         else
         {
-            musicInfosToReadQuery = musicInfosToReadQuery.OrderByDescending(e => e.Id);
-            if (request.ReferenceId is not null)
+            if (request.Asc)
             {
-                musicInfosToReadQuery = musicInfosToReadQuery.Where(e =>
-                    e.Id < request.ReferenceId
-                );
+                musicInfosToReadQuery = musicInfosToReadQuery.OrderBy(e => e.Id);
+                if (request.ReferenceId is not null)
+                {
+                    musicInfosToReadQuery = musicInfosToReadQuery.Where(e =>
+                        e.Id > request.ReferenceId
+                    );
+                }
+            }
+            else
+            {
+                musicInfosToReadQuery = musicInfosToReadQuery.OrderByDescending(e => e.Id);
+                if (request.ReferenceId is not null)
+                {
+                    musicInfosToReadQuery = musicInfosToReadQuery.Where(e =>
+                        e.Id < request.ReferenceId
+                    );
+                }
             }
         }
         var musicListsToRead = await musicInfosToReadQuery
