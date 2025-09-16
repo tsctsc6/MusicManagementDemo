1869fffdab692abe063dae6ded02cae462cc1319
tsctsc6 <tsctsc6@126.com>
2025-09-16 14:04:46 +0800

    更改添加歌曲到歌单的逻辑

--- MusicManagementDemo.Application/UseCase/Music/AddMusicInfoToMusicList/AddMusicInfoToMusicListCommandHandler.cs
+++ MusicManagementDemo.Application/UseCase/Music/AddMusicInfoToMusicList/AddMusicInfoToMusicListCommandHandler.cs
@@ -3,9 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using MusicManagementDemo.Abstractions;
 using MusicManagementDemo.Abstractions.IDbContext;
 using MusicManagementDemo.Application.Responses;
+using MusicManagementDemo.Domain.Entity.Music;
 
 namespace MusicManagementDemo.Application.UseCase.Music.AddMusicInfoToMusicList;
 
+/// <summary>
+/// 添加一首歌曲到歌单的末尾。
+/// </summary>
+/// <param name="dbContext"></param>
 internal sealed class AddMusicInfoToMusicListCommandHandler(IMusicAppDbContext dbContext)
     : IRequestHandler<AddMusicInfoToMusicListCommand, IServiceResult>
 {
@@ -22,11 +27,49 @@ internal sealed class AddMusicInfoToMusicListCommandHandler(IMusicAppDbContext d
         {
             return ServiceResult.Err(404, ["没有找到歌单"]);
         }
-        await dbContext.MusicInfoMusicListMap.AddAsync(
-            new() { MusicListId = request.MusicListId, MusicInfoId = request.MusicInfoId },
+
+        // 歌单中是否存在该歌曲
+        if (
+            await dbContext.MusicInfoMusicListMap.AnyAsync(
+                e => e.MusicListId == request.MusicListId && e.MusicInfoId == request.MusicInfoId,
+                cancellationToken: cancellationToken
+            )
+        )
+        {
+            return ServiceResult.Err(404, ["该歌曲已存在该歌单中"]);
+        }
+
+        // 查询歌单最后的歌曲
+        var lastMusicInfoMap = await dbContext
+            .MusicInfoMusicListMap.Where(e =>
+                e.MusicListId == request.MusicListId && e.NextId == null
+            )
+            .SingleOrDefaultAsync(cancellationToken: cancellationToken);
+        var expectedSubmitCount = 1;
+        await using var transaction = await dbContext.Database.BeginTransactionAsync(
             cancellationToken
         );
-        await dbContext.SaveChangesAsync(cancellationToken);
+        var musicInfoMapToAdd = new MusicInfoMusicListMap
+        {
+            MusicListId = request.MusicListId,
+            MusicInfoId = request.MusicInfoId,
+        };
+        if (lastMusicInfoMap is not null)
+        {
+            lastMusicInfoMap.NextId = request.MusicInfoId;
+            dbContext.MusicInfoMusicListMap.Update(lastMusicInfoMap);
+            musicInfoMapToAdd.PrevId = lastMusicInfoMap.MusicInfoId;
+            expectedSubmitCount = 2;
+        }
+
+        await dbContext.MusicInfoMusicListMap.AddAsync(musicInfoMapToAdd, cancellationToken);
+        var submitCount = await dbContext.SaveChangesAsync(cancellationToken);
+        if (submitCount != expectedSubmitCount)
+        {
+            return ServiceResult.Err(503, ["添加歌曲失败"]);
+        }
+
+        await transaction.CommitAsync(cancellationToken);
         return ServiceResult.Ok();
     }
 }
