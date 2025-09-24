e4dc18d0b83bfd8ac6cbdb27931642e8e900eb60
tsctsc6 <tsctsc6@126.com>
2025-09-24 17:54:22 +0800

    实现接口类

---
+++ FunctionalTesting.Infrastructure/JobHandler/FileEnumerator.cs
@@ -0,0 +1,15 @@
+﻿using MusicManagementDemo.Abstractions;
+
+namespace FunctionalTesting.Infrastructure.JobHandler;
+
+public class FileEnumerator : IFileEnumerator
+{
+    public IEnumerable<string> EnumerateFiles(
+        DirectoryInfo rootDir,
+        string searchPattern,
+        SearchOption searchOption
+    )
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ FunctionalTesting.Infrastructure/JobHandler/MusicInfoParser.cs
@@ -0,0 +1,18 @@
+﻿using MusicManagementDemo.Abstractions;
+using MusicManagementDemo.Domain.DomainEvents;
+using RustSharp;
+
+namespace FunctionalTesting.Infrastructure.JobHandler;
+
+public class MusicInfoParser : IMusicInfoParser
+{
+    public Task<Result<MusicFileFoundEventItem, string>> ParseMusicInfoAsync(
+        string fullPath,
+        int storageId,
+        string storagePath,
+        CancellationToken cancellationToken = default
+    )
+    {
+        throw new NotImplementedException();
+    }
+}
