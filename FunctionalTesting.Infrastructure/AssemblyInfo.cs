1f9472f6620a8d7b4ba33884e673009df4169495
tsctsc6 <tsctsc6@126.com>
2025-09-24 17:54:22 +0800

    实现接口类

--- FunctionalTesting.Infrastructure/AssemblyInfo.cs
+++ FunctionalTesting.Infrastructure/AssemblyInfo.cs
@@ -1,5 +1,15 @@
-﻿namespace FunctionalTesting.Infrastructure;
+﻿using FunctionalTesting.Infrastructure.JobHandler;
+using Microsoft.Extensions.DependencyInjection;
+using MusicManagementDemo.Abstractions;
+
+namespace FunctionalTesting.Infrastructure;
 
 public static class AssemblyInfo
 {
+    public static IServiceCollection AddTestInfrastructure(this IServiceCollection services)
+    {
+        services.AddSingleton<IFileEnumerator, FileEnumerator>();
+        services.AddSingleton<IMusicInfoParser, MusicInfoParser>();
+        return services;
+    }
 }
