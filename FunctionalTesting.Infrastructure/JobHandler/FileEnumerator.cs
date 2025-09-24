d2f07bd5839ad82bbbf0bfa5efa756ff2ffdfd3b
tsctsc6 <tsctsc6@126.com>
2025-09-24 18:18:27 +0800

    实现接口类

--- FunctionalTesting.Infrastructure/JobHandler/FileEnumerator.cs
+++ FunctionalTesting.Infrastructure/JobHandler/FileEnumerator.cs
@@ -1,6 +1,14 @@
-﻿using MusicManagementDemo.Abstractions;
+﻿using Bogus;
+using MusicManagementDemo.Abstractions;
 
 namespace FunctionalTesting.Infrastructure.JobHandler;
+
+internal sealed class MusicFile
+{
+    public string Title { get; set; } = string.Empty;
+    public string Album { get; set; } = string.Empty;
+    public string Artist { get; set; } = string.Empty;
+}
 
 public class FileEnumerator : IFileEnumerator
 {
@@ -10,6 +18,12 @@ public class FileEnumerator : IFileEnumerator
         SearchOption searchOption
     )
     {
-        throw new NotImplementedException();
+        var musicFiles = new Faker<MusicFile>()
+            .RuleFor(x => x.Title, f => f.Name.LastName())
+            .RuleFor(x => x.Album, f => f.Music.Genre())
+            .RuleFor(x => x.Artist, f => f.Name.FirstName())
+            .Generate(12)
+            .Select(f => Path.Combine(rootDir.FullName, f.Artist, f.Album, f.Title));
+        return musicFiles;
     }
 }
