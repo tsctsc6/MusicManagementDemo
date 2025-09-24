1babbbde39ecd9109b7a1fad69d81fe4b945c557
tsctsc6 <tsctsc6@126.com>
2025-09-24 17:17:55 +0800

    添加测试项目

---
+++ FunctionalTesting/Startup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FunctionalTesting;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        return;
+    }
+}
