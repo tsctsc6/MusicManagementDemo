bf27d94b38d72b0173dfc398f9e83d369661c1cc
tsctsc6 <tsctsc6@126.com>
2025-09-24 17:54:58 +0800

    注入依赖

--- FunctionalTesting/Startup.cs
+++ FunctionalTesting/Startup.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FunctionalTesting.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MusicManagementDemo.Application;
+using MusicManagementDemo.DbInfrastructure;
 
 namespace FunctionalTesting;
 
@@ -6,6 +10,13 @@ public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
-        return;
+        var configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddJsonFile(
+            "appsettings.Testing.json",
+            optional: false,
+            reloadOnChange: true
+        );
+        var configuration = configurationBuilder.Build();
+        services.AddDbInfrastructure(configuration).AddTestInfrastructure().AddApplication();
     }
 }
--- FunctionalTesting/UnitTest1.cs
+++ FunctionalTesting/UnitTest1.cs
@@ -1,9 +1,17 @@
-﻿namespace FunctionalTesting;
+﻿using MusicManagementDemo.Abstractions.IDbContext;
+
+namespace FunctionalTesting;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    private readonly IMusicAppDbContext _dbContext;
+
+    public UnitTest1(IMusicAppDbContext dbContext)
     {
+        _dbContext = dbContext;
+        _dbContext.Database.EnsureCreated();
     }
+
+    [Fact]
+    public void Test1() { }
 }
