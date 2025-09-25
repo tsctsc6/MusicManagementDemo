2c3abbdd06ed5ceb9772aa25c4a5575900b0ba40
tsctsc6 <tsctsc6@126.com>
2025-09-25 17:42:02 +0800

    添加测试代码

--- FunctionalTesting/Identity/RegisterTest.cs
+++ FunctionalTesting/Identity/RegisterTest.cs
@@ -5,7 +5,7 @@ namespace FunctionalTesting.Identity;
 public class RegisterTest : BaseTestingClass
 {
     [Fact]
-    public async Task Test()
+    public async Task Success()
     {
         var result = await mediator.Send(
             new RegisterCommand(Email: "aaa@aaa.com", UserName: "aaa", Password: "Abc@123"),
@@ -13,5 +13,31 @@ public class RegisterTest : BaseTestingClass
         );
         Assert.NotNull(result);
         Assert.Equal(200, result.Code);
+    }
+
+    [Fact]
+    public async Task Repeatedly()
+    {
+        await mediator.Send(
+            new RegisterCommand(Email: "aaa@aaa.com", UserName: "aaa", Password: "Abc@123"),
+            TestContext.Current.CancellationToken
+        );
+        var result = await mediator.Send(
+            new RegisterCommand(Email: "aaa@aaa.com", UserName: "aaa", Password: "Abc@123"),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.NotEqual(200, result.Code);
+    }
+
+    [Fact]
+    public async Task SimplePassword()
+    {
+        var result = await mediator.Send(
+            new RegisterCommand(Email: "aaa@aaa.com", UserName: "aaa", Password: "12345678"),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.NotEqual(200, result.Code);
     }
 }
