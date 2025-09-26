e06f09b453e63421d05a2b53ebb0a3a4332afc50
tsctsc6 <tsctsc6@126.com>
2025-09-26 13:22:26 +0800

    编写获取属性的帮助类

---
+++ FunctionalTesting/PropertyHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace FunctionalTesting;
+
+public static class PropertyHelper
+{
+    public static object? GetProperty(this object obj, string propertyName)
+    {
+        return obj.GetType().GetProperty(propertyName)?.GetValue(obj);
+    }
+}
