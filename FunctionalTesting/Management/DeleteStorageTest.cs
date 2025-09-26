f1b0f5a04fce0ef01d1b526475db7dcb2c9c5c07
tsctsc6 <tsctsc6@126.com>
2025-09-26 16:21:45 +0800

    编写测试代码

---
+++ FunctionalTesting/Management/DeleteStorageTest.cs
@@ -0,0 +1,34 @@
+﻿using MusicManagementDemo.Application.UseCase.Management.CreateStorage;
+using MusicManagementDemo.Application.UseCase.Management.DeleteStorage;
+
+namespace FunctionalTesting.Management;
+
+public class DeleteStorageTest : BaseTestingClass
+{
+    [Fact]
+    public async Task Normal()
+    {
+        var createStorageResult = await mediator.Send(
+            new CreateStorageCommand("Test", "X:\\storage1"),
+            TestContext.Current.CancellationToken
+        );
+        var storageId = (int)createStorageResult.Data!;
+        var result = await mediator.Send(
+            new DeleteStorageCommand(storageId),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.Equal(200, result.Code);
+    }
+
+    [Fact]
+    public async Task StorageNotExist()
+    {
+        var result = await mediator.Send(
+            new DeleteStorageCommand(114514),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.NotEqual(200, result.Code);
+    }
+}
