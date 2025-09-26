3796f03acbaeefefa18edf3fc6172d126d87f888
tsctsc6 <tsctsc6@126.com>
2025-09-26 15:05:33 +0800

    fix: 参数传递错误

--- FunctionalTesting/Management/CancelJob.cs
+++ FunctionalTesting/Management/CancelJob.cs
@@ -1,6 +1,8 @@
-﻿using MusicManagementDemo.Application.UseCase.Management.CancelJob;
+﻿using System.Text.Json.Nodes;
+using MusicManagementDemo.Application.UseCase.Management.CancelJob;
 using MusicManagementDemo.Application.UseCase.Management.CreateJob;
 using MusicManagementDemo.Application.UseCase.Management.CreateStorage;
+using MusicManagementDemo.Application.UseCase.Management.ReadJob;
 using MusicManagementDemo.Domain.Entity.Management;
 
 namespace FunctionalTesting.Management;
@@ -16,12 +18,33 @@ public class CancelJob : BaseTestingClass
         );
         var storageId = (int)createStorageResult.Data!;
         var createJobResult = await mediator.Send(
-            new CreateJobCommand(JobType.ScanIncremental, "ddd", $"{{\"storageId\": {storageId}}}"),
+            new CreateJobCommand(
+                JobType.ScanIncremental,
+                "ddd",
+                JsonNode.Parse($"{{\"storageId\": {storageId}}}")!
+            ),
             TestContext.Current.CancellationToken
         );
         var jobId = (long)createJobResult.Data?.GetProperty("JobId")!;
+        await Task.Delay(TimeSpan.FromSeconds(1), TestContext.Current.CancellationToken);
+        var readJobResult = await mediator.Send(
+            new ReadJobQuery(jobId),
+            TestContext.Current.CancellationToken
+        );
         var result = await mediator.Send(
             new CancelJobCommand(jobId),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.Equal(200, result.Code);
+        // 不知道为什么在单元测试环境不成功，项目实际运行起来就成功。
+    }
+
+    [Fact]
+    public async Task NotExist()
+    {
+        var result = await mediator.Send(
+            new CancelJobCommand(114514),
             TestContext.Current.CancellationToken
         );
         Assert.NotNull(result);
