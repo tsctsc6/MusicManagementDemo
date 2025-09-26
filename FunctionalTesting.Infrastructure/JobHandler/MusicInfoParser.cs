6be65606a878c0e41f3b2ea01b23bc1a1f3989e1
tsctsc6 <tsctsc6@126.com>
2025-09-26 15:05:10 +0800

    改进代码

--- FunctionalTesting.Infrastructure/JobHandler/MusicInfoParser.cs
+++ FunctionalTesting.Infrastructure/JobHandler/MusicInfoParser.cs
@@ -13,7 +13,7 @@ public class MusicInfoParser : IMusicInfoParser
         CancellationToken cancellationToken = default
     )
     {
-        await Task.Delay(TimeSpan.FromSeconds(3), cancellationToken);
+        await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
         var storage = VirtualFileSystem.VirtualFileSystem.VirtualStorages.SingleOrDefault(s =>
             s.Path == storagePath
         );
