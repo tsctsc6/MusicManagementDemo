242dfe3695fa638e3dd236d4294c60650a6fedfa
tsctsc6 <tsctsc6@126.com>
2025-09-26 11:04:26 +0800

    添加注释

--- FunctionalTesting/Startup.cs
+++ FunctionalTesting/Startup.cs
@@ -8,6 +8,11 @@ namespace FunctionalTesting;
 
 public static class Startup
 {
+    /// <summary>
+    /// 在 appsettings.Testing.json 中， ConnectionStrings:Default 中的连接字符串，不要指定 Databse ，
+    /// 程序会自动加上一个随机的数据库名称。
+    /// </summary>
+    /// <returns></returns>
     public static IServiceProvider ConfigureMyServices()
     {
         IServiceCollection servicesBuilder = new ServiceCollection();
