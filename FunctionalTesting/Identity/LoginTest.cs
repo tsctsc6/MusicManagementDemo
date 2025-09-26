b512b715f6984d775b906c882d21c90d0928b35f
tsctsc6 <tsctsc6@126.com>
2025-09-26 11:13:26 +0800

    添加测试

---
+++ FunctionalTesting/Identity/LoginTest.cs
@@ -0,0 +1,37 @@
+﻿using MusicManagementDemo.Application.UseCase.Identity.Login;
+using MusicManagementDemo.Application.UseCase.Identity.Register;
+
+namespace FunctionalTesting.Identity;
+
+public class LoginTest : BaseTestingClass
+{
+    [Fact]
+    public async Task Normal()
+    {
+        await mediator.Send(
+            new RegisterCommand(Email: "aaa@aaa.com", UserName: "aaa", Password: "Abc@123"),
+            TestContext.Current.CancellationToken
+        );
+        var result = await mediator.Send(
+            new LoginCommand(Email: "aaa@aaa.com", Password: "Abc@123"),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.Equal(200, result.Code);
+    }
+
+    [Fact]
+    public async Task PasswordError()
+    {
+        await mediator.Send(
+            new RegisterCommand(Email: "aaa@aaa.com", UserName: "aaa", Password: "Abc@123"),
+            TestContext.Current.CancellationToken
+        );
+        var result = await mediator.Send(
+            new LoginCommand(Email: "aaa@aaa.com", Password: "Abc@123!"),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.NotEqual(200, result.Code);
+    }
+}
