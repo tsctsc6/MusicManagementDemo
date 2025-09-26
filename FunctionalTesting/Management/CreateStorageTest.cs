2563d6584b8bcf25699f1646d31d765ded1a16a6
tsctsc6 <tsctsc6@126.com>
2025-09-26 11:32:27 +0800

    添加测试代码

---
+++ FunctionalTesting/Management/CreateStorageTest.cs
@@ -0,0 +1,18 @@
+﻿using MusicManagementDemo.Application.UseCase.Identity.Logout;
+using MusicManagementDemo.Application.UseCase.Management.CreateStorage;
+
+namespace FunctionalTesting.Management;
+
+public class CreateStorageTest : BaseTestingClass
+{
+    [Fact]
+    public async Task Normal()
+    {
+        var result = await mediator.Send(
+            new CreateStorageCommand("Test", "X:\\storage1"),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.Equal(200, result.Code);
+    }
+}
