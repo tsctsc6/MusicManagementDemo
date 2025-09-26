f036074dfe9462befeff5021108a87c9f6c5291d
tsctsc6 <tsctsc6@126.com>
2025-09-26 13:22:31 +0800

    编写测试代码

---
+++ FunctionalTesting/Management/CancelJob.cs
@@ -0,0 +1,29 @@
+﻿using MusicManagementDemo.Application.UseCase.Management.CancelJob;
+using MusicManagementDemo.Application.UseCase.Management.CreateJob;
+using MusicManagementDemo.Application.UseCase.Management.CreateStorage;
+using MusicManagementDemo.Domain.Entity.Management;
+
+namespace FunctionalTesting.Management;
+
+public class CancelJob : BaseTestingClass
+{
+    [Fact]
+    public async Task Normal()
+    {
+        var createStorageResult = await mediator.Send(
+            new CreateStorageCommand("Test", "X:\\storage1"),
+            TestContext.Current.CancellationToken
+        );
+        var storageId = (int)createStorageResult.Data!;
+        var createJobResult = await mediator.Send(
+            new CreateJobCommand(JobType.ScanIncremental, "ddd", $"{{\"storageId\": {storageId}}}"),
+            TestContext.Current.CancellationToken
+        );
+        var jobId = (long)createJobResult.Data?.GetProperty("JobId")!;
+        var result = await mediator.Send(
+            new CancelJobCommand(jobId),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+    }
+}
