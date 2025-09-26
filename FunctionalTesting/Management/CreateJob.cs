373941650b30c81b658fa93e22499bbb8d7328be
tsctsc6 <tsctsc6@126.com>
2025-09-26 16:14:09 +0800

    编写测试代码

--- FunctionalTesting/Management/CreateJob.cs
+++ FunctionalTesting/Management/CreateJob.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json.Nodes;
 using MusicManagementDemo.Application.UseCase.Management.CreateJob;
 using MusicManagementDemo.Application.UseCase.Management.CreateStorage;
+using MusicManagementDemo.Application.UseCase.Management.ReadJob;
 using MusicManagementDemo.Domain.Entity.Management;
 
 namespace FunctionalTesting.Management;
@@ -25,5 +26,58 @@ public class CreateJob : BaseTestingClass
         );
         Assert.NotNull(result);
         Assert.Equal(200, result.Code);
+    }
+
+    [Fact]
+    public async Task StorageNotExist()
+    {
+        var createJobResult = await mediator.Send(
+            new CreateJobCommand(
+                JobType.ScanIncremental,
+                "ddd",
+                JsonNode.Parse($"{{\"storageId\": 114514}}")!
+            ),
+            TestContext.Current.CancellationToken
+        );
+        var jobId = (long)createJobResult.Data?.GetProperty("JobId")!;
+        await Task.Delay(TimeSpan.FromSeconds(6), TestContext.Current.CancellationToken);
+        var result = await mediator.Send(
+            new ReadJobQuery(jobId),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        var completedAt = result.Data?.GetProperty("CompletedAt");
+        var success = result.Data?.GetProperty("Success");
+        Assert.NotNull(completedAt);
+        Assert.Equal(false, success);
+    }
+
+    [Fact]
+    public async Task StoragePathNotFound()
+    {
+        var createStorageResult = await mediator.Send(
+            new CreateStorageCommand("Test", "X:\\storage13"),
+            TestContext.Current.CancellationToken
+        );
+        var storageId = (int)createStorageResult.Data!;
+        var createJobResult = await mediator.Send(
+            new CreateJobCommand(
+                JobType.ScanIncremental,
+                "ddd",
+                JsonNode.Parse($"{{\"storageId\": {storageId}}}")!
+            ),
+            TestContext.Current.CancellationToken
+        );
+        var jobId = (long)createJobResult.Data?.GetProperty("JobId")!;
+        await Task.Delay(TimeSpan.FromSeconds(6), TestContext.Current.CancellationToken);
+        var result = await mediator.Send(
+            new ReadJobQuery(jobId),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        var completedAt = result.Data?.GetProperty("CompletedAt");
+        var success = result.Data?.GetProperty("Success");
+        Assert.NotNull(completedAt);
+        Assert.Equal(false, success);
     }
 }
