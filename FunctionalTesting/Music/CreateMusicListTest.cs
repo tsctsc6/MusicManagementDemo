6ee77aa57cfaadc0a031c9a0fc350a05a4a11d4a
tsctsc6 <tsctsc6@126.com>
2025-09-26 11:46:59 +0800

    添加测试代码

---
+++ FunctionalTesting/Music/CreateMusicListTest.cs
@@ -0,0 +1,43 @@
+﻿using MusicManagementDemo.Application.UseCase.Identity.Register;
+using MusicManagementDemo.Application.UseCase.Music.CreateMusicList;
+
+namespace FunctionalTesting.Music;
+
+public class CreateMusicListTest : BaseTestingClass
+{
+    [Fact]
+    public async Task Normal()
+    {
+        var regResult = await mediator.Send(
+            new RegisterCommand(Email: "aaa@aaa.com", UserName: "aaa", Password: "Abc@123"),
+            TestContext.Current.CancellationToken
+        );
+        var userId = Guid.Parse(regResult.Data!.ToString()!);
+        var result = await mediator.Send(
+            new CreateMusicListCommand(userId, "New MusicList"),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.Equal(200, result.Code);
+    }
+
+    [Fact]
+    public async Task Repeatedly()
+    {
+        var regResult = await mediator.Send(
+            new RegisterCommand(Email: "aaa@aaa.com", UserName: "aaa", Password: "Abc@123"),
+            TestContext.Current.CancellationToken
+        );
+        var userId = Guid.Parse(regResult.Data!.ToString()!);
+        await mediator.Send(
+            new CreateMusicListCommand(userId, "New MusicList"),
+            TestContext.Current.CancellationToken
+        );
+        var result = await mediator.Send(
+            new CreateMusicListCommand(userId, "New MusicList"),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.Equal(200, result.Code);
+    }
+}
