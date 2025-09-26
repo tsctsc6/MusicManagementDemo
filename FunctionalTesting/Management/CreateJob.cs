03f6cb0930760ab3ab2ba35114b6ee1ab503ec6c
tsctsc6 <tsctsc6@126.com>
2025-09-26 15:40:03 +0800

    编写测试代码

---
+++ FunctionalTesting/Management/CreateJob.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Nodes;
+using MusicManagementDemo.Application.UseCase.Management.CreateJob;
+using MusicManagementDemo.Application.UseCase.Management.CreateStorage;
+using MusicManagementDemo.Domain.Entity.Management;
+
+namespace FunctionalTesting.Management;
+
+public class CreateJob : BaseTestingClass
+{
+    [Fact]
+    public async Task Normal()
+    {
+        var createStorageResult = await mediator.Send(
+            new CreateStorageCommand("Test", "X:\\storage1"),
+            TestContext.Current.CancellationToken
+        );
+        var storageId = (int)createStorageResult.Data!;
+        var result = await mediator.Send(
+            new CreateJobCommand(
+                JobType.ScanIncremental,
+                "ddd",
+                JsonNode.Parse($"{{\"storageId\": {storageId}}}")!
+            ),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.Equal(200, result.Code);
+    }
+}
