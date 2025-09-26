c81cb409201a84b830571780bb29d6f2af27ffd3
tsctsc6 <tsctsc6@126.com>
2025-09-26 15:23:42 +0800

    编写测试代码

--- FunctionalTesting/Management/CancelJob.cs
+++ FunctionalTesting/Management/CancelJob.cs
@@ -27,17 +27,12 @@ public class CancelJob : BaseTestingClass
         );
         var jobId = (long)createJobResult.Data?.GetProperty("JobId")!;
         await Task.Delay(TimeSpan.FromSeconds(1), TestContext.Current.CancellationToken);
-        var readJobResult = await mediator.Send(
-            new ReadJobQuery(jobId),
-            TestContext.Current.CancellationToken
-        );
         var result = await mediator.Send(
             new CancelJobCommand(jobId),
             TestContext.Current.CancellationToken
         );
         Assert.NotNull(result);
         Assert.Equal(200, result.Code);
-        // 不知道为什么在单元测试环境不成功，项目实际运行起来就成功。
     }
 
     [Fact]
