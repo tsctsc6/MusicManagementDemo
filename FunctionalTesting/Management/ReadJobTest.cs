d79693a57d688b0a83f7617c12283f707b840a3e
tsctsc6 <tsctsc6@126.com>
2025-09-26 17:37:04 +0800

    编写测试代码

---
+++ FunctionalTesting/Management/ReadJobTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json.Nodes;
+using MusicManagementDemo.Application.UseCase.Management.CreateJob;
+using MusicManagementDemo.Application.UseCase.Management.CreateStorage;
+using MusicManagementDemo.Application.UseCase.Management.ReadJob;
+using MusicManagementDemo.Domain.Entity.Management;
+
+namespace FunctionalTesting.Management;
+
+public class ReadJobTest : BaseTestingClass
+{
+    [Fact]
+    public async Task Normal()
+    {
+        var createStorageResult = await mediator.Send(
+            new CreateStorageCommand("Test", "X:\\storage1"),
+            TestContext.Current.CancellationToken
+        );
+        var storageId = (int)createStorageResult.Data!;
+        var createJobResult = await mediator.Send(
+            new CreateJobCommand(
+                JobType.ScanIncremental,
+                "ddd",
+                JsonNode.Parse($"{{\"storageId\": {storageId}}}")!
+            ),
+            TestContext.Current.CancellationToken
+        );
+        var jobId = (long)createJobResult.Data?.GetProperty("JobId")!;
+        await Task.Delay(TimeSpan.FromSeconds(1), TestContext.Current.CancellationToken);
+        var result = await mediator.Send(
+            new ReadJobQuery(jobId),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.Equal(200, result.Code);
+    }
+
+    [Fact]
+    public async Task NotExist()
+    {
+        var result = await mediator.Send(
+            new ReadJobQuery(114514),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.NotEqual(200, result.Code);
+    }
+}
