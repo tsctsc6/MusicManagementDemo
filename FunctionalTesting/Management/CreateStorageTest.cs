cfebf52a91cbad71c3a6db2cedfd0feb258fdbc8
tsctsc6 <tsctsc6@126.com>
2025-09-26 11:38:31 +0800

    添加测试代码

--- FunctionalTesting/Management/CreateStorageTest.cs
+++ FunctionalTesting/Management/CreateStorageTest.cs
@@ -15,4 +15,19 @@ public class CreateStorageTest : BaseTestingClass
         Assert.NotNull(result);
         Assert.Equal(200, result.Code);
     }
+
+    [Fact]
+    public async Task Repeatedly()
+    {
+        await mediator.Send(
+            new CreateStorageCommand("Test", "X:\\storage1"),
+            TestContext.Current.CancellationToken
+        );
+        var result = await mediator.Send(
+            new CreateStorageCommand("Test", "X:\\storage1"),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.NotEqual(200, result.Code);
+    }
 }
