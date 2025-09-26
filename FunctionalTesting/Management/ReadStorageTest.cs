f586e87114e1563914433b377506a8165eea820b
tsctsc6 <tsctsc6@126.com>
2025-09-26 17:41:06 +0800

    编写测试代码

---
+++ FunctionalTesting/Management/ReadStorageTest.cs
@@ -0,0 +1,34 @@
+﻿using MusicManagementDemo.Application.UseCase.Management.CreateStorage;
+using MusicManagementDemo.Application.UseCase.Management.ReadStorage;
+
+namespace FunctionalTesting.Management;
+
+public class ReadStorageTest : BaseTestingClass
+{
+    [Fact]
+    public async Task Normal()
+    {
+        var createStorageResult = await mediator.Send(
+            new CreateStorageCommand("Test", "X:\\storage1"),
+            TestContext.Current.CancellationToken
+        );
+        var storageId = (int)createStorageResult.Data!;
+        var result = await mediator.Send(
+            new ReadStorageQuery(storageId),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.Equal(200, result.Code);
+    }
+
+    [Fact]
+    public async Task NotExist()
+    {
+        var result = await mediator.Send(
+            new ReadStorageQuery(114514),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.NotEqual(200, result.Code);
+    }
+}
