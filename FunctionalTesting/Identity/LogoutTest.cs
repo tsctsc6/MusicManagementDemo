e1ac93ad8c9ed236400e68926a17aca689da4a31
tsctsc6 <tsctsc6@126.com>
2025-09-26 11:25:32 +0800

    添加测试

---
+++ FunctionalTesting/Identity/LogoutTest.cs
@@ -0,0 +1,27 @@
+﻿using MusicManagementDemo.Application.UseCase.Identity.Login;
+using MusicManagementDemo.Application.UseCase.Identity.Logout;
+using MusicManagementDemo.Application.UseCase.Identity.Register;
+
+namespace FunctionalTesting.Identity;
+
+public class LogoutTest : BaseTestingClass
+{
+    [Fact]
+    public async Task Normal()
+    {
+        var tempResult = await mediator.Send(
+            new RegisterCommand(Email: "aaa@aaa.com", UserName: "aaa", Password: "Abc@123"),
+            TestContext.Current.CancellationToken
+        );
+        await mediator.Send(
+            new LoginCommand(Email: "aaa@aaa.com", Password: "Abc@123"),
+            TestContext.Current.CancellationToken
+        );
+        var result = await mediator.Send(
+            new LogoutCommand(UserId: Guid.Parse(tempResult.Data!.ToString()!)),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.Equal(200, result.Code);
+    }
+}
