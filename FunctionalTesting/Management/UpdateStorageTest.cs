065ec22d8f034b3ceb2178edc9ad87cc3a966205
tsctsc6 <tsctsc6@126.com>
2025-09-26 17:47:28 +0800

    编写测试代码

---
+++ FunctionalTesting/Management/UpdateStorageTest.cs
@@ -0,0 +1,34 @@
+﻿using MusicManagementDemo.Application.UseCase.Management.CreateStorage;
+using MusicManagementDemo.Application.UseCase.Management.UpdateStorage;
+
+namespace FunctionalTesting.Management;
+
+public class UpdateStorageTest : BaseTestingClass
+{
+    [Fact]
+    public async Task Normal()
+    {
+        var createStorageResult = await mediator.Send(
+            new CreateStorageCommand("Test", "X:\\storage1"),
+            TestContext.Current.CancellationToken
+        );
+        var storageId = (int)createStorageResult.Data!;
+        var result = await mediator.Send(
+            new UpdateStorageCommand(storageId, "Test2", "X:\\storage1"),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.Equal(200, result.Code);
+    }
+
+    [Fact]
+    public async Task NotExist()
+    {
+        var result = await mediator.Send(
+            new UpdateStorageCommand(114514, "Test2", "X:\\storage1"),
+            TestContext.Current.CancellationToken
+        );
+        Assert.NotNull(result);
+        Assert.NotEqual(200, result.Code);
+    }
+}
