b428fc8506a8b49c70481a50b382e9e455ba6fed
tsctsc6 <tsctsc6@126.com>
2025-09-29 11:05:47 +0800

    优化代码

--- FunctionalTesting/Identity/LoginTest.cs
+++ FunctionalTesting/Identity/LoginTest.cs
@@ -5,13 +5,21 @@ namespace FunctionalTesting.Identity;
 
 public class LoginTest : BaseTestingClass
 {
-    [Fact]
-    public async Task Normal()
+    private Guid userId;
+
+    private async Task PrepareAsync()
     {
-        await mediator.Send(
+        var regResult = await mediator.Send(
             new RegisterCommand(Email: "aaa@aaa.com", UserName: "aaa", Password: "Abc@123"),
             TestContext.Current.CancellationToken
         );
+        userId = Guid.Parse(regResult.Data!.ToString()!);
+    }
+
+    [Fact]
+    public async Task Normal()
+    {
+        await PrepareAsync();
         var result = await mediator.Send(
             new LoginCommand(Email: "aaa@aaa.com", Password: "Abc@123"),
             TestContext.Current.CancellationToken
--- FunctionalTesting/Identity/LogoutTest.cs
+++ FunctionalTesting/Identity/LogoutTest.cs
@@ -6,19 +6,27 @@ namespace FunctionalTesting.Identity;
 
 public class LogoutTest : BaseTestingClass
 {
-    [Fact]
-    public async Task Normal()
+    private Guid userId;
+
+    private async Task PrepareAsync()
     {
-        var tempResult = await mediator.Send(
+        var regResult = await mediator.Send(
             new RegisterCommand(Email: "aaa@aaa.com", UserName: "aaa", Password: "Abc@123"),
             TestContext.Current.CancellationToken
         );
+        userId = Guid.Parse(regResult.Data!.ToString()!);
+    }
+
+    [Fact]
+    public async Task Normal()
+    {
+        await PrepareAsync();
         await mediator.Send(
             new LoginCommand(Email: "aaa@aaa.com", Password: "Abc@123"),
             TestContext.Current.CancellationToken
         );
         var result = await mediator.Send(
-            new LogoutCommand(UserId: Guid.Parse(tempResult.Data!.ToString()!)),
+            new LogoutCommand(userId),
             TestContext.Current.CancellationToken
         );
         Assert.NotNull(result);
