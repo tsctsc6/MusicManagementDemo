4fb10c60fb9a564a560099f6314b8fabef1666c2
tsctsc6 <tsctsc6@126.com>
2025-10-09 11:59:49 +0800

    添加注释

--- MusicManagementDemo.Domain/Entity/Music/MusicInfo.cs
+++ MusicManagementDemo.Domain/Entity/Music/MusicInfo.cs
@@ -2,12 +2,24 @@
 
 namespace MusicManagementDemo.Domain.Entity.Music;
 
+/// <summary>
+/// 音乐信息
+/// </summary>
 public sealed class MusicInfo
 {
+    /// <summary>
+    /// 唯一标识
+    /// </summary>
     public Guid Id { get; set; }
 
+    /// <summary>
+    /// 音乐名称
+    /// </summary>
     public string Title { get; set; } = string.Empty;
 
+    /// <summary>
+    /// 参与创作的艺术家
+    /// </summary>
     public string Artist { get; set; } = string.Empty;
 
     public string Album { get; set; } = string.Empty;
@@ -17,8 +29,14 @@ public sealed class MusicInfo
     /// </summary>
     public string FilePath { get; set; } = string.Empty;
 
+    /// <summary>
+    /// 对应的存储 id
+    /// </summary>
     public int StorageId { get; set; }
 
+    /// <summary>
+    /// 音乐名称的文本搜索向量，是存储生成列，由 pgsql 根据音乐名称自动计算。
+    /// </summary>
 #pragma warning disable CS8618
     // ReSharper disable once InconsistentNaming
     public NpgsqlTsVector TitleTSV { get; set; }
--- MusicManagementDemo.Domain/Entity/Music/MusicInfoMusicListMap.cs
+++ MusicManagementDemo.Domain/Entity/Music/MusicInfoMusicListMap.cs
@@ -1,14 +1,32 @@
 ﻿namespace MusicManagementDemo.Domain.Entity.Music;
 
+/// <summary>
+/// 音乐信息和歌单的对应表
+/// </summary>
 public class MusicInfoMusicListMap
 {
+    /// <summary>
+    /// 音乐信息 id
+    /// </summary>
     public Guid MusicInfoId { get; set; }
 
+    /// <summary>
+    /// 歌单 id
+    /// </summary>
     public Guid MusicListId { get; set; }
 
+    /// <summary>
+    /// 前一首音乐的 id, null 表示这是第一首
+    /// </summary>
     public Guid? PrevId { get; set; }
 
+    /// <summary>
+    /// 后一首音乐的 id, null 表示这是最后一首
+    /// </summary>
     public Guid? NextId { get; set; }
 
+    /// <summary>
+    /// 加入歌单时间
+    /// </summary>
     public DateTime CreatedAt { get; set; }
 }
