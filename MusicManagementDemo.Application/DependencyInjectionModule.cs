0d2567e14cd019dffcf5772308c03f5954d1543b
tsctsc6 <tsctsc6@126.com>
2025-11-02 21:00:36 +0800

    添加疑似 Mediator 的 bug 的注释

--- MusicManagementDemo.Application/DependencyInjectionModule.cs
+++ MusicManagementDemo.Application/DependencyInjectionModule.cs
@@ -18,7 +18,9 @@ public static class DependencyInjectionModule
             options.ServiceLifetime = ServiceLifetime.Scoped;
             options.GenerateTypesAsInternal = true;
             options.NotificationPublisherType = typeof(ForeachAwaitPublisher);
-            //options.Assemblies = [typeof(DependencyInjectionModule)];
+            // 不知道为什么，如果指定要扫描的 Assembly ， INotificationHandler 不会出现在 Mediator.g.cs 中
+            // 新建最小项目做实验，无法复现此问题。
+            // options.Assemblies = [typeof(DependencyInjectionModule).Assembly];
             options.PipelineBehaviors =
             [
                 typeof(ExceptionHandlingBehavior<,>),
