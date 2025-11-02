2097637dbb98819c50ff2e425daa61c1ba6c808d
tsctsc6 <tsctsc6@126.com>
2025-11-02 15:45:14 +0800

    编写 Music 模块的准备帮助方法

--- FunctionalTesting/Provisions/MusicProvision.cs
+++ FunctionalTesting/Provisions/MusicProvision.cs
@@ -1,6 +1,43 @@
-﻿namespace FunctionalTesting.Provisions;
+﻿using MediatR;
+using MusicManagementDemo.Application.UseCase.Music.AddMusicInfoToMusicList;
+using MusicManagementDemo.Application.UseCase.Music.CreateMusicList;
+using MusicManagementDemo.Application.UseCase.Music.ReadAllMusicInfo;
+
+namespace FunctionalTesting.Provisions;
 
 public static class MusicProvision
 {
-    
+    public static async Task<Guid> CreateMusicListAsync(
+        IMediator mediator,
+        CreateMusicListCommand command,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var createMusicListResult = await mediator.Send(command, cancellationToken);
+        return Guid.Parse(createMusicListResult.Data!.GetPropertyValue("Id")!.ToString()!);
+    }
+
+    public static async Task AddMusicInfoToMusicListAsync(
+        IMediator mediator,
+        AddMusicInfoToMusicListCommand command,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var createMusicListResult = await mediator.Send(command, cancellationToken);
+    }
+
+    public static async Task<Guid[]> ReadAllMusicInfoAsync(
+        IMediator mediator,
+        ReadAllMusicInfoQuery query,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var result = await mediator.Send(query, cancellationToken);
+        return
+        [
+            .. (result.Data! as IEnumerable<object>)!.Select(o =>
+                Guid.Parse(o.GetPropertyValue("Id")!.ToString()!)
+            ),
+        ];
+    }
 }
