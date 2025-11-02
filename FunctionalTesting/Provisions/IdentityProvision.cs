5b6570a2c95d795ec972b8dcb54f8f87fdc51ae7
tsctsc6 <tsctsc6@126.com>
2025-11-02 15:12:52 +0800

    添加 cancellationToken

--- FunctionalTesting/Provisions/IdentityProvision.cs
+++ FunctionalTesting/Provisions/IdentityProvision.cs
@@ -8,7 +8,7 @@ public static class IdentityProvision
     public static async Task<Guid> RegisterAsync(
         IMediator mediator,
         RegisterCommand command,
-        CancellationToken cancellationToken
+        CancellationToken cancellationToken = default
     )
     {
         var result = await mediator.Send(command, cancellationToken);
