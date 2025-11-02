a1d86d111f7164febf4169437fe8f93baa672b0b
tsctsc6 <tsctsc6@126.com>
2025-11-02 15:24:27 +0800

    编写 Management 模块的准备帮助方法

--- FunctionalTesting/Provisions/ManagementProvision.cs
+++ FunctionalTesting/Provisions/ManagementProvision.cs
@@ -1,6 +1,28 @@
-﻿namespace FunctionalTesting.Provisions;
+﻿using MediatR;
+using MusicManagementDemo.Application.UseCase.Management.CreateJob;
+using MusicManagementDemo.Application.UseCase.Management.CreateStorage;
+
+namespace FunctionalTesting.Provisions;
 
 public static class ManagementProvision
 {
-    
+    public static async Task<int> CreateStorageAsync(
+        IMediator mediator,
+        CreateStorageCommand command,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var result = await mediator.Send(command, cancellationToken);
+        return (int)result.Data!.GetPropertyValue("Id")!;
+    }
+
+    public static async Task<long> CreateJobAsync(
+        IMediator mediator,
+        CreateJobCommand command,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var result = await mediator.Send(command, cancellationToken);
+        return (long)result.Data?.GetPropertyValue("JobId")!;
+    }
 }
