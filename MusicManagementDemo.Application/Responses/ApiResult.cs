ce84cc8d5a6dfaf0540d74c2ac9345efdeb6c5a5
tsctsc6 <tsctsc6@126.com>
2025-11-17 20:43:23 +0800

    把 ServiceResult 重命名为 ApiResult

---
+++ MusicManagementDemo.Application/Responses/ApiResult.cs
@@ -0,0 +1,20 @@
+﻿namespace MusicManagementDemo.Application.Responses;
+
+public sealed record ApiResult<T>(bool IsFinish, int? Code, T? Data, string? ErrorMessage)
+    where T : class
+{
+    public static ApiResult<T> Ok(T? data = null)
+    {
+        return new ApiResult<T>(IsFinish: true, Code: 200, Data: data, ErrorMessage: null);
+    }
+
+    public static ApiResult<T> Err(int code, string? errorMessage = null)
+    {
+        return new ApiResult<T>(
+            IsFinish: true,
+            Code: code,
+            Data: null,
+            ErrorMessage: errorMessage
+        );
+    }
+}
