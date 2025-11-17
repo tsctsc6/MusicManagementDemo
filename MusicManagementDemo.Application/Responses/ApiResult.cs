e3f1f0227289b35bf1b35d02642101d5011d0a48
tsctsc6 <tsctsc6@126.com>
2025-11-17 21:12:38 +0800

    修改 ApiResult 类

--- MusicManagementDemo.Application/Responses/ApiResult.cs
+++ MusicManagementDemo.Application/Responses/ApiResult.cs
@@ -1,20 +1,15 @@
 ﻿namespace MusicManagementDemo.Application.Responses;
 
-public sealed record ApiResult<T>(bool IsFinish, int? Code, T? Data, string? ErrorMessage)
+public record ApiResult<T>(bool IsFinish, int? Code, T? Data, string? ErrorMessage)
     where T : class
 {
-    public static ApiResult<T> Ok(T? data = null)
+    public static ApiResult<T> Ok(T data)
     {
-        return new ApiResult<T>(IsFinish: true, Code: 200, Data: data, ErrorMessage: null);
+        return new ApiResult<T>(true, 200, data, null);
     }
 
     public static ApiResult<T> Err(int code, string? errorMessage = null)
     {
-        return new ApiResult<T>(
-            IsFinish: true,
-            Code: code,
-            Data: null,
-            ErrorMessage: errorMessage
-        );
+        return new ApiResult<T>(true, code, null, errorMessage);
     }
 }
