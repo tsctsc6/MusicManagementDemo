7b76cc193d30cbf52375ec1ad7309f8ac9e7c65c
tsctsc6 <tsctsc6@126.com>
2025-11-18 14:26:24 +0800

    修改 ValidationBehavior.cs 为 ApiReuestValidationBehavior.cs ；简化泛型约束

--- MusicManagementDemo.Application/PipelineMediators/ApiRequestValidationBehavior.cs
+++ MusicManagementDemo.Application/PipelineMediators/ApiRequestValidationBehavior.cs
@@ -5,17 +5,16 @@ using MusicManagementDemo.Application.Responses;
 
 namespace MusicManagementDemo.Application.PipelineMediators;
 
-internal sealed class ValidationBehavior<TRequest, TResponse, T>(
+internal sealed class ApiRequestValidationBehavior<TRequest, T>(
     IEnumerable<IValidator<TRequest>> validators,
-    ILogger<ValidationBehavior<TRequest, TResponse, T>> logger
-) : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : IRequest<TResponse>
-    where TResponse : ApiResult<T>
+    ILogger<ApiRequestValidationBehavior<TRequest, T>> logger
+) : IPipelineBehavior<TRequest, ApiResult<T>>
+    where TRequest : IRequest<ApiResult<T>>
     where T : class
 {
-    public async ValueTask<TResponse> Handle(
+    public async ValueTask<ApiResult<T>> Handle(
         TRequest request,
-        MessageHandlerDelegate<TRequest, TResponse> next,
+        MessageHandlerDelegate<TRequest, ApiResult<T>> next,
         CancellationToken cancellationToken
     )
     {
@@ -32,7 +31,7 @@ internal sealed class ValidationBehavior<TRequest, TResponse, T>(
             string[] errorMessages = [.. failures.Select(f => f.ErrorMessage)];
             // ReSharper disable once CoVariantArrayConversion
             logger.LogError("Validation failed: {@errorMessages}", errorMessages);
-            return (TResponse)ApiResult<T>.Err(406, string.Join("\n", errorMessages));
+            return ApiResult<T>.Err(406, string.Join("\n", errorMessages));
         }
 
         return await next(request, cancellationToken);
