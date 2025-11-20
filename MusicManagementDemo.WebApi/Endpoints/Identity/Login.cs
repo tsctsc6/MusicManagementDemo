efc5d4830b56a9ba48a987e79d8aefb9cfe5d144
tsctsc6 <tsctsc6@126.com>
2025-11-20 17:33:48 +0800

    使 Request 类在类的外面；去除不必要的参数

--- MusicManagementDemo.WebApi/Endpoints/Identity/Login.cs
+++ MusicManagementDemo.WebApi/Endpoints/Identity/Login.cs
@@ -3,16 +3,16 @@ using MusicManagementDemo.Application.UseCase.Identity.Login;
 
 namespace MusicManagementDemo.WebApi.Endpoints.Identity;
 
+internal sealed record LoginRequest(string Email, string Password);
+
 [RegisterTransient<IEndpoint>(Duplicate = DuplicateStrategy.Append, Tags = InjectioTags.Endpoint)]
 internal sealed class Login : IEndpoint
 {
-    private sealed record Request(string Email, string UserName, string Password);
-
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
         app.MapPost(
             "api/identity/login",
-            async (Request request, IMediator mediator, CancellationToken cancellationToken) =>
+            async (LoginRequest request, IMediator mediator, CancellationToken cancellationToken) =>
             {
                 var result = await mediator.Send(
                     new LoginCommand(Email: request.Email, Password: request.Password),
