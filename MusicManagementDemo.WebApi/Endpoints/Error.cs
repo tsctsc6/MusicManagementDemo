90103632986f5e1d21c2b934698478f2c44ddd7a
tsctsc6 <tsctsc6@126.com>
2025-11-22 19:39:59 +0800

    全局异常处理，仅返回 HTTP 500

--- MusicManagementDemo.WebApi/Endpoints/Error.cs
+++ MusicManagementDemo.WebApi/Endpoints/Error.cs
@@ -5,31 +5,7 @@ internal sealed class Error : IEndpoint
 {
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapGet(
-                "/error",
-                () =>
-                    TypedResults.InternalServerError(
-                        new
-                        {
-                            IsFinish = true,
-                            Code = "500",
-                            ErrorMessage = "Internal Server Error",
-                        }
-                    )
-            )
-            .WithName("ErrorGet");
-        app.MapPost(
-                "/error",
-                () =>
-                    TypedResults.InternalServerError(
-                        new
-                        {
-                            IsFinish = true,
-                            Code = "500",
-                            ErrorMessage = "Internal Server Error",
-                        }
-                    )
-            )
-            .WithName("ErrorPost");
+        app.MapGet("/error", TypedResults.InternalServerError).WithName("ErrorGet");
+        app.MapPost("/error", TypedResults.InternalServerError).WithName("ErrorPost");
     }
 }
