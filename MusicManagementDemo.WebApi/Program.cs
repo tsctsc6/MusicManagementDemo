262509c78f75676758973ad16411fd92e62a81e2
tsctsc6 <tsctsc6@126.com>
2025-11-28 13:58:31 +0800

    改变依赖注入的位置

--- MusicManagementDemo.WebApi/Program.cs
+++ MusicManagementDemo.WebApi/Program.cs
@@ -1,62 +1,20 @@
-using Microsoft.AspNetCore.Cors.Infrastructure;
 using MusicManagementDemo.Application;
 using MusicManagementDemo.Infrastructure.Core;
 using MusicManagementDemo.Infrastructure.Database;
+using MusicManagementDemo.WebApi;
 using MusicManagementDemo.WebApi.Endpoints;
 using Serilog;
 using InjectioTags = MusicManagementDemo.WebApi.InjectioTags;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 builder
     .Services.AddApplication()
     .AddDatabaseInfrastructure(builder.Configuration)
     .AddSharedInfrastructure(builder.Configuration)
     .AddRealInfrastructure()
+    .AddWebApi(builder.Configuration)
     .AddWebApiByInjectio(InjectioTags.Endpoint);
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(
-        "FromConfiguration",
-        policy =>
-        {
-            var corsConfiguration =
-                builder.Configuration.GetSection("CorsPolicy").Get<CorsPolicy>()
-                ?? new CorsPolicy();
-            if (corsConfiguration.Origins.Contains("*"))
-            {
-                policy.AllowAnyOrigin();
-            }
-            else
-            {
-                policy.WithOrigins([.. corsConfiguration.Origins]);
-            }
-
-            if (corsConfiguration.Methods.Contains("*"))
-            {
-                policy.AllowAnyMethod();
-            }
-            else
-            {
-                policy.WithMethods([.. corsConfiguration.Methods]);
-            }
-
-            if (corsConfiguration.Headers.Contains("*"))
-            {
-                policy.AllowAnyHeader();
-            }
-            else
-            {
-                policy.WithHeaders([.. corsConfiguration.Headers]);
-            }
-        }
-    );
-});
 
 var app = builder.Build();
 
