0e6cc0f9701c0d0b8fedb0ea8d575742731bcd9e
tsctsc6 <tsctsc6@126.com>
2026-02-20 22:46:18 +0800

    重写 Application 层相关接口

--- MusicManagementDemo.Abstractions/IDbContext/IMusicAppDbContext.cs
+++ MusicManagementDemo.Abstractions/IDbContext/IMusicAppDbContext.cs
@@ -8,11 +8,4 @@ public interface IMusicAppDbContext : IDbContext
     public DbSet<MusicInfo> MusicInfos { get; }
     public DbSet<MusicList> MusicLists { get; }
     public DbSet<MusicInfoMusicListMap> MusicInfoMusicListMaps { get; }
-
-    public IQueryable<MusicInfo> GetMusicInfoInMusicList(
-        Guid musicListId,
-        Guid? referenceId,
-        int pageSize,
-        bool asc
-    );
 }
